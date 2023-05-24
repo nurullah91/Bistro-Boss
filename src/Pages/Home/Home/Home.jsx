6ee42f75c17680fe311889abcac32f16dad0ca19
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-24 18:47:55 +0600

    Nav bar, slider, swiper, footer added

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import Banner from "../Banner/Banner";
+import Menu from "../Menu/Menu";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Banner></Banner>
+            <Menu></Menu>
+        </div>
+    );
+};
+
+export default Home;
