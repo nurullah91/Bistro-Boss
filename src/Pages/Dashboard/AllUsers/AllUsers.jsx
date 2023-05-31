79c92622a06dd8c87d24988602011cd6091b96a5
nurullah91 <mdnurullah.ptk@gmail.com>
2023-05-31 15:12:41 +0600

    jwt token seved to local storage

--- src/Pages/Dashboard/AllUsers/AllUsers.jsx
+++ src/Pages/Dashboard/AllUsers/AllUsers.jsx
@@ -29,7 +29,7 @@ const AllUsers = () => {
             })
 
     }
-    const { data: users = [], refetch } = useQuery(['users'], async () => {
+    const { data: users = []} = useQuery(['users'], async () => {
         const res = await fetch('http://localhost:5000/users')
         return res.json();
 
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { createContext } from 'react';
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from '../Firebase/firebase.config';
+import axios from 'axios';
 export const AuthContext = createContext(null);
 
 const auth = getAuth(app);
@@ -44,6 +45,19 @@ const AuthProvider = ({children}) => {
     useEffect( ()=>{
         const unsubscribe = onAuthStateChanged(auth, currentUser=>{
             setUser(currentUser);
+
+            if(currentUser){
+                axios.post('http://localhost:5000/jwt', {email: currentUser.email})
+                .then(data=>{
+                    console.log(data.data.token);
+                    localStorage.setItem("token", data.data.token)
+
+                })
+            }
+
+            else{
+                localStorage.removeItem("token")
+            }
             setLoading(false);
         });
 
