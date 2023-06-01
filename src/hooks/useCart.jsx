e0342455b5a5ef3d9d34ee4c0ab36312e838ce12
nurullah91 <mdnurullah.ptk@gmail.com>
2023-06-01 08:19:00 +0600

    jwt token verify done

--- src/hooks/useCart.jsx
+++ src/hooks/useCart.jsx
@@ -5,11 +5,17 @@ import { AuthContext } from '../Provider/AuthProvider';
 
 const useCart = () =>{
     const {user} = useContext(AuthContext);
+    const token = localStorage.getItem('access-token');
     
+
     const { refetch, data: cart = []  } = useQuery({
         queryKey: ['cart', user?.email],
         queryFn:  async () => {
-            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+            const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`, {
+                headers: {
+                    authorization:`bearer ${token}`
+                }
+            })
             return res.json();
           },
       })
